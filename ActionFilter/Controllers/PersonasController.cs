5a04c0a3ce7ae7ff60101a6861823f6bb169e1b0
Ivan Sanchez <ivanvictoria1996@gmail.com>
2020-02-20 21:07:38 +0100

    Add project files.

---
+++ ActionFilter/Controllers/PersonasController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionFilter.Controllers
+{
+    public class PersonasController : Controller
+    {
+        // GET: Personas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ActionFilter/Models/Coche.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActionFilter.Models
+{
+    public class Coche
+    {
+
+        public Coche(int id, String marca, String modelo, string color)
+        {
+            this.IdCoche = id;
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.Color = color;
+        }
+
+
+        public int IdCoche { get; set; }
+
+        public String Marca { get; set; }
+
+        public String Modelo { get; set; }
+
+        public String Color { get; set; }
+    }
+}
